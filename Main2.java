cf87d44bfe9d1c3312b6130639d67b1d712ff0c0
Lena <42959967+lenadyomkina@users.noreply.github.com>
2018-09-05 12:11:34 +0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,15 @@
+package com.gmail.ld;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Triangle triangleOne = new Triangle(3, 4, 5);
+		System.out.println(triangleOne.area());
+		
+		Triangle triangleTwo = new Triangle(4, 8, 9);
+		System.out.println(triangleTwo.area());
+	}
+
+}
